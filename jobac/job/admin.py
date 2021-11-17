b0e3107298f44fe6402d0a38cfd24688784af7c9
MehdiAzari <lolaccmahdi2@gmail.com>
2021-11-17 15:23:01 +0330

    Removed incorrect folders

--- jobac/job/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from Job.models import JobOffer, Proposal
-
-# Register your models here.
-admin.site.register(JobOffer)
-admin.site.register(Proposal)
--- jobac/job/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwnerOfJob(permissions.BasePermission):
-
-    def has_object_permission(self, request,_, obj):
-        return obj.employer.user == request.user
-
